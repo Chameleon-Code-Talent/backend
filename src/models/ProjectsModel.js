408038159b0bd686c3a13c424a64c2633284ed03
igoryan260 <igor-yanbarbosa@hotmail.com>
2024-01-17 20:07:50 -0300

    feat: Creating data collection 'projects' and validation in mongodb

---
+++ src/models/ProjectsModel.js
@@ -0,0 +1,42 @@
+import { Int32 } from "mongodb";
+import { run, closeBd, database } from "../../config/dbConnection.js";
+
+const conn = () => { run().catch(console.dir); };
+
+async function createCollectionValidate() {
+    if (!database.collection("users")) {
+        database.createCollection("projects", {
+            validator: {
+                $jsonSchema: {
+                    bsonType: "object",
+                    title: "Users object validation",
+                    required: ["thumbnail", "title", "technologies", "description"],
+                    properties: {
+                        thumbnail: {
+                            bsonType: String,
+                            description: "'thumbnail' is required"
+                        },
+                        title: {
+                            bsonType: String,
+                            description: "'title' is required"
+                        },
+                        project_images: {
+                            bsonType: String,
+                            description: "'project_images' filed not found"
+                        },
+                        technologies: {
+                            bsonType: String,
+                            description: "'technologies' is required"
+                        },
+                        description: {
+                            bsonType: Int32,
+                            description: "'description' is required"
+                        },
+                    }
+                }
+            }
+        })
+    };
+};
+
+export default createCollectionValidate;
