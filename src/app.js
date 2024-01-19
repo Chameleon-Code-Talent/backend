7fcb990d9d2bc0a472bf97981383a3a6ee26fa93
igoryan260 <igor-yanbarbosa@hotmail.com>
2024-01-19 20:03:46 -0300

    feat: Using import model to create models schemas

--- src/app.js
+++ src/app.js
@@ -2,10 +2,10 @@ import express from "express";
 import dotenv from "dotenv/config.js";
 import usersRoute from "./routes/usersRoutes.js";
 import projectsRoute from "./routes/projectsRoutes.js"
-import createCollectionValidate from "./models/UsersModel.js";
+import createCollectionValidate from "./models/SkillsModel.js";
 const app = express();
 
-
+//await createCollectionValidate();
 
 //importing routes middlewares 
 app.use(express.json());
