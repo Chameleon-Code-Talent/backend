33beb5291fbf401b5120ae4f28370d76a951f7ce
igoryan260 <igor-yanbarbosa@hotmail.com>
2024-01-21 11:17:27 -0300

    feat: new routes projects

--- src/routes/projectsRoutes.js
+++ src/routes/projectsRoutes.js
@@ -1,9 +1,17 @@
 import express from "express";
+import { run, closeBd, database } from "../../config/dbConnection.js";
+import ProjectsController from "../controller/ProjectsController.js";
 
 const route = express.Router();
 
 route.get("/", (req, res) => {
-    res.send("you are on the test page projects")
+    res.send("you are on the test page Project")
 });
+
+route.get("/projects", ProjectsController.searchProjects);
+route.get("/projects/:id", ProjectsController.searchProjectById);
+route.post("/projects", ProjectsController.registerProject);
+route.put("/projects/:id", ProjectsController.updateProject);
+route.delete("/projects/:id", ProjectsController.deleteProject);
 
 export default route;
