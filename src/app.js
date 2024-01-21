6c54c4f4e18cb2222267ae9d8a3c4f9772775df4
igoryan260 <igor-yanbarbosa@hotmail.com>
2024-01-21 11:16:28 -0300

    feat: New middlewares

--- src/app.js
+++ src/app.js
@@ -2,7 +2,8 @@ import express from "express";
 import dotenv from "dotenv/config.js";
 import usersRoute from "./routes/usersRoutes.js";
 import projectsRoute from "./routes/projectsRoutes.js"
-import createCollectionValidate from "./models/SkillsModel.js";
+import skillsRoute from "./routes/skillsRoutes.js";
+import servicesRoute from "./routes/servicesRoute.js";
 const app = express();
 
 //await createCollectionValidate();
@@ -11,5 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(usersRoute);
 app.use(projectsRoute);
+app.use(skillsRoute);
+app.use(servicesRoute);
 
 export default app;
