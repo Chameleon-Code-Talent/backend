7ef0e399c0a8dc77c72d7884de5a6de1130527b0
igoryan260 <igor-yanbarbosa@hotmail.com>
2024-01-23 18:59:43 -0300

    feat: run index.js (all routes)

--- src/app.js
+++ src/app.js
@@ -1,18 +1,10 @@
 import express from "express";
 import dotenv from "dotenv/config.js";
-import usersRoute from "./routes/usersRoutes.js";
-import projectsRoute from "./routes/projectsRoutes.js"
-import skillsRoute from "./routes/skillsRoutes.js";
-import servicesRoute from "./routes/servicesRoute.js";
+import routes from "./routes/index.js";
+
 const app = express();
-
-//await createCollectionValidate();
-
-//importing routes middlewares 
+//importing json middlewares 
 app.use(express.json());
-app.use(usersRoute);
-app.use(projectsRoute);
-app.use(skillsRoute);
-app.use(servicesRoute);
+routes(app);
 
 export default app;
