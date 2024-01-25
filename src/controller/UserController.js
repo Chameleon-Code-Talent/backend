c79c5a7ff23e57cfd7d55d31bbf28b152a2295ed
igoryan260 <igor-yanbarbosa@hotmail.com>
2024-01-25 20:50:16 -0300

    feat: new authenticate controller and verify token test

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -1,8 +1,9 @@
 import { run, closeBd, database } from "../../config/dbConnection.js";
 import { ObjectId } from "mongodb";
-const collection = database.collection("users");
 
+const collection = database.collection("users");
 class UserController {
+
     static async searchUsers(req, res) {
         try {
             await run();
@@ -29,27 +30,56 @@ class UserController {
     static async registerUser(req, res) {
         try {
             const newUser = req.body;
-            console.log(newUser)
             await run();
             const result = await collection.insertOne(newUser);
             res.status(200).json(result);
             await closeBd();
         } catch (err) {
-
             //catching validation error
             console.log(err.errInfo.details.schemaRulesNotSatisfied[0]);
             res.status(500).json({ message: "Server error", error: err.message });
         };
     };
 
+    /*controller for authentication user*/
+    static async userAuthentication(req, res, next) {
+        try {
+            const email = req.body.email
+            const password = req.body.password
+
+            //verify empty content or null
+            if (!email || !password) {
+                return res.status(400).json({ message: "The fields 'email' and 'password' is required!" })
+            }
+
+            const userFound = await collection.findOne({ "email": email, "password": password });
+
+            if (userFound) {
+                res.status(200).json(userFound);
+            } else {
+                res.status(404).json({ message: "User not found" });
+            }
+
+        } catch (err) {
+            console.log(err)
+            res.status(500).json(err)
+        }
+    }
+
     static async updateUser(req, res) {
         try {
             await run();
             const id_user = new ObjectId(req.params.id);
-            const result = await collection.updateOne({ "_id": id_user }, { $set: req.body });
+            const verify = await verifyToken("itokenteste")
+            if (verify === true) {
+                const result = await collection.updateOne({ "_id": id_user }, { $set: req.body });
+            } else {
+                throw new Error("Acesso Negado");
+            };
             await closeBd();
             res.status(200).json({ mensage: "User updated successfully" });
         } catch (err) {
+            console.error(err);
             res.status(500).json({ message: "Server error", error: err });
         };
     };
@@ -67,4 +97,14 @@ class UserController {
     };
 };
 
-export default UserController;
+async function verifyToken(meutoken) {
+
+    const secretToken = process.env.TOKEN
+    console.log(secretToken)
+
+    if (meutoken == secretToken) {
+        return true;
+    }
+};
+
+export { UserController, verifyToken };
