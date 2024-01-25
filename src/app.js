0a14e5a801edca5fd1e27c915b9dca1184e6a69b
igoryan260 <igor-yanbarbosa@hotmail.com>
2024-01-25 20:50:42 -0300

    feat: add middleware use in the app.js

--- src/app.js
+++ src/app.js
@@ -2,9 +2,11 @@ import express from "express";
 import dotenv from "dotenv/config.js";
 import routes from "./routes/index.js";
 
+
 const app = express();
 //importing json middlewares 
 app.use(express.json());
 routes(app);
+
 
 export default app;
