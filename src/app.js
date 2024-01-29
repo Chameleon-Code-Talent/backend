eaac76f647e81709e69b6a98fdad5248d54c2eb8
igoryan260 <igor-yanbarbosa@hotmail.com>
2024-01-29 18:56:04 -0300

    feat: importing middlewares of errors

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import dotenv from "dotenv/config.js";
 import routes from "./routes/index.js";
+import errorManipulation from "./middlewares/errorManipulation.js";
 
 
 const app = express();
 //importing json middlewares 
 app.use(express.json());
+app.use(errorManipulation);
 routes(app);
 
 
