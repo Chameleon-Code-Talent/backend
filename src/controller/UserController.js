dfa1802f818a5d9b878a76c0d2c6f98af324fd35
igoryan260 <igor-yanbarbosa@hotmail.com>
2024-01-29 18:55:40 -0300

    refactoring: importing middlewares and usage in methods of the classes

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -1,21 +1,26 @@
 import { run, closeBd, database } from "../../config/dbConnection.js";
-import { ObjectId } from "mongodb";
+import { BSON, ObjectId } from "mongodb";
+import jwt from "jsonwebtoken";
+import verifyToken from "../middlewares/verifyToken.js";
+import bcrypt from "bcrypt";
 
 const collection = database.collection("users");
 class UserController {
 
-    static async searchUsers(req, res) {
+    static async searchUsers(req, res, next) {
         try {
             await run();
             const result = await collection.find().toArray();
             await closeBd();
             res.status(200).json(result);
         } catch (err) {
-            res.status(500).json({ message: "Server error", error: err });
+            console.log(err)
+            await closeBd();
+            next(err)
         };
     };
 
-    static async searchUserById(req, res) {
+    static async searchUserById(req, res, next) {
         try {
             await run();
             const user = new ObjectId(req.params.id);
@@ -23,68 +28,100 @@ class UserController {
             await closeBd();
             res.status(200).json(result);
         } catch (err) {
-            res.status(500).json({ message: "Server error", error: err });
+            console.log(err)
+            await closeBd();
+            next(err)
         };
     };
 
-    static async registerUser(req, res) {
+    static async registerUser(req, res, next) {
         try {
+            //generate salt 12 bits and password criptograph
+            const salt = await bcrypt.genSalt(12);
+            const passwordHash = await bcrypt.hash(req.body.password, salt)
+
+            req.body.password = passwordHash;
+
             const newUser = req.body;
             await run();
             const result = await collection.insertOne(newUser);
+
+            //response of result operation
             res.status(200).json(result);
             await closeBd();
         } catch (err) {
-            //catching validation error
-            console.log(err.errInfo.details.schemaRulesNotSatisfied[0]);
-            res.status(500).json({ message: "Server error", error: err.message });
+            //catching validation error            
+            await closeBd();
+            next(err)
         };
     };
 
     /*controller for authentication user*/
     static async userAuthentication(req, res, next) {
         try {
-            const email = req.body.email
-            const password = req.body.password
+            const email = req.body.email;
+            const password = req.body.password;
 
             //verify empty content or null
             if (!email || !password) {
                 return res.status(400).json({ message: "The fields 'email' and 'password' is required!" })
             }
 
-            const userFound = await collection.findOne({ "email": email, "password": password });
+            await run();
 
+            const userFound = await collection.findOne({ "email": email });
+            //comparations hashs
+
+            const checkPassword = await bcrypt.compare(password, userFound.password)
+            console.log(checkPassword);
+
+            //validate password
+            if (!checkPassword) {
+                return res.status(404).json({ message: "User not found" })
+            }
+
+            //user found or not found
             if (userFound) {
-                res.status(200).json(userFound);
+                const id = String(userFound._id)
+                const itoken = jwt.sign({ id }, process.env.SECRET_KEY)
+                await closeBd();
+                res.status(200).json(itoken);
             } else {
+                await closeBd();
                 res.status(404).json({ message: "User not found" });
             }
 
         } catch (err) {
             console.log(err)
-            res.status(500).json(err)
+            await closeBd();
+            next(err)
         }
     }
 
-    static async updateUser(req, res) {
+    static async updateUser(req, res, next) {
         try {
-            await run();
             const id_user = new ObjectId(req.params.id);
-            const verify = await verifyToken("itokenteste")
-            if (verify === true) {
-                const result = await collection.updateOne({ "_id": id_user }, { $set: req.body });
+
+            await run();
+
+            const result = await collection.updateOne({ "_id": id_user }, { $set: req.body });
+
+            if (result.modifiedCount === 1) {
+                //if is user was modified
+                res.status(200).json({ mensage: "User updated successfully" });
             } else {
-                throw new Error("Acesso Negado");
-            };
+                //if is user was modified
+                res.status(404).json({ message: "User not modified" });
+            }
             await closeBd();
-            res.status(200).json({ mensage: "User updated successfully" });
         } catch (err) {
-            console.error(err);
-            res.status(500).json({ message: "Server error", error: err });
+            console.error(err)
+            await closeBd();
+            next(err)
         };
     };
 
-    static async deleteUser(req, res) {
+    static async deleteUser(req, res, next) {
         try {
             await run();
             const user = new ObjectId(req.params.id);
@@ -92,19 +129,14 @@ class UserController {
             await closeBd();
             res.status(200).json({ mensage: "User deleted successfully" });
         } catch (err) {
-            res.status(500).json({ message: "Server error", error: err });
+            console.log(err)
+            await closeBd();
+            next(err)
         };
     };
 };
 
-async function verifyToken(meutoken) {
 
-    const secretToken = process.env.TOKEN
-    console.log(secretToken)
+export default UserController;
 
-    if (meutoken == secretToken) {
-        return true;
-    }
-};
-
-export { UserController, verifyToken };
+//65b2b428cb56900027f01799
