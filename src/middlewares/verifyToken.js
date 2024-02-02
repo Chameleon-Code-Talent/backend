5f09326f1d4150acc913a0af3e94e4dfbfc76d59
igoryan260 <igor-yanbarbosa@hotmail.com>
2024-02-02 18:31:45 -0300

    refact: atualizate token authentication

--- src/middlewares/verifyToken.js
+++ src/middlewares/verifyToken.js
@@ -1,10 +1,24 @@
 import jwt, { decode } from 'jsonwebtoken';
+import { run, closeBd, database } from "../../config/dbConnection.js";
+import { ObjectId } from 'mongodb';
 
 //verify token authentication
 async function verifyToken(req, res, next) {
     try {
+        //get unique token of the user
+        const collection = database.collection("users");
+        const id_user = new ObjectId(req.params.id);
+
+        await run()
+        const uniqueToken = await collection.findOne({ "_id": id_user }, { projection: { "token": 1, "_id": 0 } })
+        await closeBd();
+
+        //tranform unique token in string
+        const stringToken = String(uniqueToken.token);
+
+        //verify token authentication
         const signature = req.headers.authorization && req.headers.authorization.split(' ')[1];
-        const token = jwt.verify(signature, process.env.SECRET_KEY)
+        const token = jwt.verify(signature, stringToken)
         if (!token) {
             return res.status(401).json({ message: "Access denied!" })
         }
